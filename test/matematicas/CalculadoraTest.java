2c8c28a950be0313192a37390f62d5546743d0de
usuario <usuario@pc20-044>
2020-11-05 09:07:22 +0100

    v1.0

---
+++ test/matematicas/CalculadoraTest.java
@@ -0,0 +1,38 @@
+package matematicas;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ulises126
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSuma() {
+        int total = 8;
+        int sum = Calculadora.suma(5, 3);
+        assertEquals(sum, total);
+    }
+    
+    @Test
+    public void testResta() {
+        int res = 8;
+        int sum = Calculadora.resta(5, 3);
+        assertEquals(sum, res);
+    }
+}
